62ba42218c2e4ee88e61a0cfcb840923444c9368
edgarcaswell <edgarcaswell@cca.edu>
2017-09-07 16:46:01 -0700

    update

--- exercise-charrette/assets/script.js
+++ exercise-charrette/assets/script.js
@@ -10,3 +10,16 @@ $(document).ready(function(){
 	});
 
 });
+
+$(document).ready(function(){
+	
+	$('#button-4').click(function() {
+		$(this).animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			fontSize: '36px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
