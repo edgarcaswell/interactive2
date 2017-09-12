4a552692ad71c846507b99f1d232a28973d070e4
edgarcaswell <edgarcaswell@cca.edu>
2017-09-11 17:55:14 -0700

    update

---
+++ exercise-charrette/script.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+	
+	$('#button-3').click(function() {
+		$(this).animate({
+			opacity: 0.4,
+			marginLeft: '0.6in',
+			fontSize: '24px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-4').click(function() {
+		$(this).animate({
+			opacity: 0,
+			marginLeft: '0.3in',
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, .5 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-5').click(function() {
+		$(this).animate({
+			opacity: 0,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-6').click(function() {
+		$(this).animate({
+			opacity: .10,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-7').click(function() {
+		$(this).animate({
+			opacity: .20,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-8').click(function() {
+		$(this).animate({
+			opacity: .30,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-9').click(function() {
+		$(this).animate({
+			opacity: .40,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+
+    $('#button-15').click(function(){
+        alert("im so proud of you.");
+    });
+});
+
+$(document).ready(function(){
+
+    $('#button-16').click(function(){
+        $('#button-16').hide(1000);
+    });
+});
+
+$(document).ready(function(){
+	
+    $('#button-17').click(function(){
+        $('#button-17').hide(500);
+        
+    });
+});
+
+$(document).ready(function(){
+	
+    $('#button-18').click(function(){
+        $('#button-18').hide();
+        
+    });
+});
+
+$(document).ready(function(){
+	
+    $('#button-33').click(function(){
+        $('#button-33').hide();
+        
+    });
+});
+
+$(document).ready(function(){
+	
+	$('#button-39').click(function() {
+		$(this).animate({
+			opacity: .50,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-40').click(function() {
+		$(this).animate({
+			opacity: .0,
+			rotate: 20,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
