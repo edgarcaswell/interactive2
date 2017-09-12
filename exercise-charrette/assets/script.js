c9af7668fbf7d518b8e1c1333322f77669ad6d2e
edgarcaswell <edgarcaswell@cca.edu>
2017-09-11 17:25:01 -0700

    update

--- exercise-charrette/assets/script.js
+++ exercise-charrette/assets/script.js
@@ -15,11 +15,11 @@ $(document).ready(function(){
 	
 	$('#button-4').click(function() {
 		$(this).animate({
-			opacity: 0.4,
-			marginLeft: '0.6in',
+			opacity: 0,
+			marginLeft: '0.3in',
 			fontSize: '50px',
 			borderWidth: '10px'
-		}, 1000 );
+		}, .5 );
 	});
 
 });
@@ -101,9 +101,49 @@ $(document).ready(function(){
 $(document).ready(function(){
 	
     $('#button-17').click(function(){
-        $('#button-17').hide(10000);
+        $('#button-17').hide(500);
         
     });
 });
 
+$(document).ready(function(){
+	
+    $('#button-18').click(function(){
+        $('#button-18').hide();
+        
+    });
+});
+
+$(document).ready(function(){
+	
+    $('#button-33').click(function(){
+        $('#button-33').hide();
+        
+    });
+});
+
+$(document).ready(function(){
+	
+	$('#button-39').click(function() {
+		$(this).animate({
+			opacity: .50,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
+
+$(document).ready(function(){
+	
+	$('#button-40').click(function() {
+		$(this).animate({
+			opacity: .0,
+			rotate: 20,
+			fontSize: '50px',
+			borderWidth: '10px'
+		}, 1000 );
+	});
+
+});
 
