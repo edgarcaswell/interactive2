49c64cadd9a6f8cf4618ba3dd561e283e574a081
edgarcaswell <edgarcaswell@cca.edu>
2017-09-28 17:21:38 -0700

    update

--- gif/assets/script.js
+++ gif/assets/script.js
@@ -1,9 +1,21 @@
 
 
-$(function(){
-    var image = new Image();
-    image.src ='http://rack.3.mshcdn.com/media/ZgkyMDEyLzEwLzE5LzExXzMzXzMzXzE3Nl9maWxlCnAJdGh1bWIJMTIwMHg5NjAwPg/462b8072';
-    $('#img').click(function(){
-       $(this).attr('src',image.src);
-    });
+$(document).ready(function() { 
+  $("#button").click(function() {
+    var src = $('#myimage').attr('src');
+
+    //if the current image is picture1.png, change it to picture2.png
+    if(src == 'http://www.libpng.org/pub/png/PngSuite/f01n2c08.png') {
+        $("#myimage").attr("src","http://www.libpng.org/pub/png/PngSuite/f02n2c08.png");
+
+    //if the current image is picture2.png, change it to picture3.png 
+    } else if(src == "http://www.libpng.org/pub/png/PngSuite/f02n2c08.png") {
+        $("#myimage").attr("src","http://www.libpng.org/pub/png/PngSuite/f03n2c08.png"); 
+    //if the current image is anything else, change it back to picture1.png
+    } else {
+        $("#myimage").attr("src","http://www.libpng.org/pub/png/PngSuite/f01n2c08.png");
+    }
+    console.log("Src is: " + $("#myimage").attr("src"));
+  })
 });
+
