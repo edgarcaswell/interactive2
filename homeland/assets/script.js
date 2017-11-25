e025058fb5e7bd40d9c05de0266718ddce336cd4
edgarcaswell <edgarcaswell@cca.edu>
2017-11-25 01:18:45 -0800

    update

---
+++ homeland/assets/script.js
@@ -0,0 +1,10 @@
+ 
+ $(document).ready(function(){
+	
+ 	$('#white').delay(500).fadeTo(7000, 0);
+
+ 	$('#title').delay(500).fadeTo(5000, 0);
+
+ 	$('#larrysultan2').delay(500).fadeTo(3800, 0);
+	
+	});
