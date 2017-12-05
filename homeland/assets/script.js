0c938512e3b84aea79c3338f856f739e3b4ffcae
edgarcaswell <edgarcaswell@cca.edu>
2017-12-04 19:37:30 -0800

    update

--- homeland/assets/script.js
+++ homeland/assets/script.js
@@ -6,5 +6,80 @@
  	$('#title').delay(500).fadeTo(5000, 0);
 
  	$('#larrysultan2').delay(500).fadeTo(3800, 0);
-	
+ 	// clip part begin
+ 		// clip homeland2 begin 
+	$("#homeland2").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 1480px 1420px)"
+		})
+	})
+	 		// clip homeland2 end 
+
+	 		// clip img7 begin 
+	$("#img7").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 1979px 2050px)"
+		})
+	})
+	 		// clip img7 end 
+
+	 		// clip img2 begin 
+	$("#img2").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 1979px 2050px)"
+		})
+	})
+	 		// clip img2 end 
+
+	 		 // clip img3 begin 
+	$("#img3").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 1188px 1434px)"
+		})
+	})
+	 		// clip img3 end 
+
+	 		// clip img8 begin 
+	$("#img8").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 638px 1620px)"
+		})
+	})
+	 		// clip img8 end 
+
+	 		// clip img4 begin 
+	$("#img4").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 1380px 1262px)"
+		})
+	})
+	 		// clip img4 end 
+			
+			
+			// clip img5 begin 
+	$("#img5").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 813px 1144px)"
+		})
+	})
+	 		// clip img5 end 
+
+	 		// clip img6 begin 
+	$("#img6").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 1691px 1573px)"
+		})
+	})
+	 		// clip img6 end 
+
+	 		// clip img1 begin 
+	$("#img1").click(function(){
+		$(this).css({
+			clipPath: "circle(3200px at 2033px 1231px)"
+		})
+	})
+	 		// clip img1 end 
+
+	 	// clip part end
+
 	});
